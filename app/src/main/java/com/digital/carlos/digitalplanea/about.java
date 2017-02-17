dfa2876e66649cfb311b43985d13dadcb3e04219
CarlosArriagaCM <carlosarriagacm@gmail.com>
2017-02-17 14:49:08 -0600

    add profiles

--- app/src/main/java/com/digital/carlos/digitalplanea/about.java
+++ app/src/main/java/com/digital/carlos/digitalplanea/about.java
@@ -5,17 +5,65 @@ package com.digital.carlos.digitalplanea;
  */
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class about extends Activity {
 
         @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
-
+            // show new activity
             setContentView(R.layout.about);
+            // set profile Vanessa Linares
+            ImageView vanessa = (ImageView)findViewById(R.id.vanessa);
+            vanessa.setOnClickListener(new View.OnClickListener(){
+                public void onClick(View v){
+                    Intent intent = new Intent();
+                    intent.setAction(Intent.ACTION_VIEW);
+                    intent.addCategory(Intent.CATEGORY_BROWSABLE);
+                    intent.setData(Uri.parse("https://www.facebook.com/vane.linaresescorcia"));
+                    startActivity(intent);
+                }
+            });
+            // set profile Carlos Arriaga
+            ImageView carlos = (ImageView)findViewById(R.id.carlos);
+            carlos.setOnClickListener(new View.OnClickListener(){
+                public void onClick(View v){
+                    Intent intent = new Intent();
+                    intent.setAction(Intent.ACTION_VIEW);
+                    intent.addCategory(Intent.CATEGORY_BROWSABLE);
+                    intent.setData(Uri.parse("https://www.facebook.com/CarlosArriagaCM"));
+                    startActivity(intent);
+                }
+            });
+            // set profile Jośe renteria
+            ImageView rente = (ImageView)findViewById(R.id.rente);
+            rente.setOnClickListener(new View.OnClickListener(){
+                public void onClick(View v){
+                    Intent intent = new Intent();
+                    intent.setAction(Intent.ACTION_VIEW);
+                    intent.addCategory(Intent.CATEGORY_BROWSABLE);
+                    intent.setData(Uri.parse("https://www.facebook.com/jose.renteria.77377692"));
+                    startActivity(intent);
+                }
+            });
+            // set profile José Loredo
+            ImageView crow = (ImageView)findViewById(R.id.crow);
+            crow.setOnClickListener(new View.OnClickListener(){
+                public void onClick(View v){
+                    Intent intent = new Intent();
+                    intent.setAction(Intent.ACTION_VIEW);
+                    intent.addCategory(Intent.CATEGORY_BROWSABLE);
+                    intent.setData(Uri.parse("https://www.facebook.com/profile.php?id=100009745005523"));
+                    startActivity(intent);
+                }
+            });
 
         }
 
-}
+} // end class
 
